ec347384ba295e91eda66fb71d2b84e727e80846
Jason R. Adams <Jason@JRAdams.me>
2026-04-10 18:37:34 -0700

    Adding some basics

---
+++ LENA/Entity/Common/AuditableEntrry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LENA.Domain.Entity.Common
+{
+    public class AuditableEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string? LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedDate { get; set; }
+
+    }
+}
