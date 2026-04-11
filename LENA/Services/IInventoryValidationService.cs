98095f37be4d60dea0da96b86387cf1269a228fb
Jason R. Adams <Jason@JRAdams.me>
2026-04-11 06:55:56 -0700

    Add Validation

---
+++ LENA/Services/IInventoryValidationService.cs
@@ -0,0 +1,20 @@
+﻿using LENA.Domain.Entity.Inventory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LENA.Domain.Services
+{
+    public interface IInventoryValidationService
+    {
+        ValidationResult ValidateRequirement(Item item);
+        ValidationResult ValidateRequirementForUpdate(Item existingItem, Item newItem);
+        List<string> GetValidationErrors(Item item);
+    }
+    public class ValidationResult
+    {
+        public bool IsValid { get; set; }
+        public List<string> Errors { get; set; } = new();
+        public List<string> Warnings { get; set; } = new();
+    }
+}
