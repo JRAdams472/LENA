cee5aafe25df86b467a5043baa52839decc1aad7
Jason R. Adams <Jason@JRAdams.me>
2026-08-31 10:30:02 -0700

    Make BaseRepository abstract shell and add explicit CRUD to Type/Vintage

--- LENA.Application/Repositories/TypeRepository.cs
+++ LENA.Application/Repositories/TypeRepository.cs
@@ -10,6 +10,50 @@ namespace LENA.Application.Repositories
         {
         }
 
+        public override async Task<TypeEntity> CreateAsync(TypeEntity entity)
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = @"INSERT INTO [Wine].[Type] (TypeName, Description, IsActive, CreatedBy, CreateDate)
+                        VALUES (@TypeName, @Description, @IsActive, @CreatedBy, @CreateDate);
+                        SELECT CAST(SCOPE_IDENTITY() as int);";
+            var id = await connection.QuerySingleAsync<int>(sql, entity);
+            entity.TypeID = id;
+            return entity;
+        }
+
+        public override async Task<TypeEntity?> GetByIdAsync(int id)
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = "SELECT * FROM [Wine].[Type] WHERE TypeID = @Id";
+            return await connection.QueryFirstOrDefaultAsync<TypeEntity>(sql, new { Id = id });
+        }
+
+        public override async Task<IReadOnlyList<TypeEntity>> ListAllAsync()
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = "SELECT * FROM [Wine].[Type] ORDER BY TypeName";
+            return (IReadOnlyList<TypeEntity>)await connection.QueryAsync<TypeEntity>(sql);
+        }
+
+        public override async Task<TypeEntity> UpdateAsync(TypeEntity entity)
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = @"UPDATE [Wine].[Type]
+                        SET TypeName = @TypeName, Description = @Description, IsActive = @IsActive,
+                            LastUpdatedBy = @LastUpdatedBy, LastUpdatedDate = @LastUpdatedDate
+                        WHERE TypeID = @TypeID";
+            await connection.ExecuteAsync(sql, entity);
+            return entity;
+        }
+
+        public override async Task<TypeEntity> DeleteAsync(TypeEntity entitey)
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = "DELETE FROM [Wine].[Type] WHERE TypeID = @TypeID";
+            await connection.ExecuteAsync(sql, new { entitey.TypeID });
+            return entitey;
+        }
+
         public override async Task<TypeEntity?> GetByNameAsync(string name)
         {
             using var connection = await _connectionFactory.CreateConnectionAsync();
--- LENA.Application/Repositories/VintageRepository.cs
+++ LENA.Application/Repositories/VintageRepository.cs
@@ -10,6 +10,55 @@ namespace LENA.Application.Repositories
         {
         }
 
+        public override async Task<Vintage> CreateAsync(Vintage entity)
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = @"INSERT INTO [Wine].[Vintage] (Year, Description, IsActive, CreatedBy, CreateDate)
+                        VALUES (@Year, @Description, @IsActive, @CreatedBy, @CreateDate);
+                        SELECT CAST(SCOPE_IDENTITY() as int);";
+            var id = await connection.QuerySingleAsync<int>(sql, entity);
+            entity.VintageID = id;
+            return entity;
+        }
+
+        public override async Task<Vintage?> GetByIdAsync(int id)
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = "SELECT * FROM [Wine].[Vintage] WHERE VintageID = @Id";
+            return await connection.QueryFirstOrDefaultAsync<Vintage>(sql, new { Id = id });
+        }
+
+        public override async Task<IReadOnlyList<Vintage>> ListAllAsync()
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = "SELECT * FROM [Wine].[Vintage] ORDER BY Year";
+            return (IReadOnlyList<Vintage>)await connection.QueryAsync<Vintage>(sql);
+        }
+
+        public override async Task<Vintage> UpdateAsync(Vintage entity)
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = @"UPDATE [Wine].[Vintage]
+                        SET Year = @Year, Description = @Description, IsActive = @IsActive,
+                            LastUpdatedBy = @LastUpdatedBy, LastUpdatedDate = @LastUpdatedDate
+                        WHERE VintageID = @VintageID";
+            await connection.ExecuteAsync(sql, entity);
+            return entity;
+        }
+
+        public override async Task<Vintage> DeleteAsync(Vintage entitey)
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            var sql = "DELETE FROM [Wine].[Vintage] WHERE VintageID = @VintageID";
+            await connection.ExecuteAsync(sql, new { entitey.VintageID });
+            return entitey;
+        }
+
+        public override async Task<Vintage?> GetByNameAsync(string name)
+        {
+            return await Task.FromResult<Vintage?>(null);
+        }
+
         public async Task<Vintage?> GetByYearAsync(int year)
         {
             using var connection = await _connectionFactory.CreateConnectionAsync();
